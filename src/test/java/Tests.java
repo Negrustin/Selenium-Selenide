c8a8546db3da011ddb2839ce7e87ed82cb9f9266
Aliaksandr Zuyeuski <negrustin87@gmail.com>
2022-08-13 07:46:36 +0200

    ci

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -21,6 +21,7 @@ public class Tests {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
+
         driver = new ChromeDriver();
     }
 
